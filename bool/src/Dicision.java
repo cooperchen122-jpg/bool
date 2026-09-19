6f67cb267762f99ff09ef45be89dfa89e9be6ee7
Cooperchen122 <Cooperchen122@gmail.com>
2026-09-19 16:08:35 +0800

    revision 2

--- bool/src/Dicision.java
+++ bool/src/Dicision.java
@@ -16,6 +16,19 @@ public class Dicision {
             }
         }
     }
+    public static String validation2(String d){
+        while(true){
+            if(d=="A"||d=="a"||d=="B"||d=="b"||d=="C"||d=="c"||d=="D"||d=="d"){
+                return d;
+            }
+            else{
+                System.out.println("Please enter A/B/C/D: ");
+                String d1=input(); 
+                String r=validation2(d1);
+                return r;
+            }
+        }
+    }
     public static String input(){
         Scanner input = new Scanner(System.in);
         String d=input.nextLine();
@@ -27,7 +40,9 @@ public class Dicision {
         System.out.println("Would you cook at home tonight?(Y/N)");
         String d1=input();
         boolean r1=validation1(d1);
-
+        System.out.println("What is your vibe this weekend? (A/B/C/D) \nA. Chill \nB. Social \nC. Productive \nD. IDK");
+        String d2=input();
+        String r2=validation2(d2);
         
     }
 }
