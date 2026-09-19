4d660f8763404589e7f74b4ab30dac543767db1b
Cooperchen122 <Cooperchen122@gmail.com>
2026-09-19 16:00:08 +0800

    revision 1

--- bool/src/Dicision.java
+++ bool/src/Dicision.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class Dicision {
-    public static boolean validation(String d){
+    public static boolean validation1(String d){
         while(true){
-            if(d.equals("Y")||d.equals("y")){
+            if(d=="Y"||d=="y"){
                 return true;
             }
-            else if(d.equals("N")||d.equals("n")){
+            else if(d=="N"||d=="n"){
                 return false;
             }
             else{
                 System.out.println("Please enter Y/N: ");
                 String d1=input(); 
-                boolean r1=validation(d1);
-                return r1;
+                boolean r=validation1(d1);
+                return r;
             }
         }
     }
@@ -22,20 +22,12 @@ public class Dicision {
         return d;
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the food advisor!");
-        System.out.println("We would help you make decisions on what food to eat next.");
-        System.out.println("Are you healthy and away from diseases?(Y/N)");
+        System.out.println("Welcome to the weekend advisor!");
+        System.out.println("We would help you make decisions on what to do this weekend.");
+        System.out.println("Would you cook at home tonight?(Y/N)");
         String d1=input();
-        boolean r1=validation(d1);
-        System.out.println("Would you like to eat cheap food or expensive food?(Y/N)");
-        String d2=input();
-        boolean r2=validation(d2);
-        System.out.println("Would you like to eat ?(Y/N)");
-        String d3=input();
-        boolean r3=validation(d3);
-        System.out.println("Are you healthy and away from diseases?(Y/N)");
-        String d4=input();
-        boolean r4=validation(d4);
+        boolean r1=validation1(d1);
+
         
     }
 }
